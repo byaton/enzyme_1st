c629566f5a2b5b6b28e0440ed9837d70ea284e28
Amit Roy <amit.roy@construction.com>
2020-10-11 19:32:08 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import EnzymeAdapter  from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = _ => shallow(<App />);
+const findByTestAttr = (wrapper, elComp) => wrapper.find(`[data-test='${elComp}']`);
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+test('renders button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+test('counter starts at 0', () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe('0');
+});
+test('clicking on button increments counter display', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+  const count = +findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe(1);
 });
